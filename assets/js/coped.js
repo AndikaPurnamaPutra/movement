51565707e09702fa2944b4657a3ffe8c74b6c35f
AndikaPurnamaPutra <andikapratamaputra849@gmail.com>
2023-09-25 12:11:30 +0700

    fixed bug: all section

--- assets/js/coped.js
+++
@@ -1,9 +0,0 @@
-function copyFunction(elementId) {
-    const copyText = document.getElementById(elementId).textContent;
-    const textArea = document.createElement("textarea");
-    textArea.textContent = copyText;
-    document.body.append(textArea);
-    textArea.select();
-    document.execCommand("copy");
-    document.body.removeChild(textArea);
-}
